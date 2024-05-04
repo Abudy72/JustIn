a8b9eaa1f54bf0f3d51c53253938bd0aaf39ba60
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-05-04 12:04:52 -0400

    Scrape experiences and education - MOBILE ONLY

--- JustIn/content.js
+++ JustIn/content.js
@@ -1,11 +1,20 @@
 async function scrapeText() {
     function experiences() {
-        const experiences = document.querySelectorAll('#app-container > section.bg-color-background-container.experience-container.py-2.pl-2.mt-1.collapsible-list-container.collapsed > ol > li')
+        const experiences = document.querySelectorAll('#app-container > section.bg-color-background-container.experience-container.py-2.pl-2.mt-1.collapsible-list-container.collapsed > ol > li');
+        // const experiences = document.querySelectorAll('span');
         experiences.forEach(element => {
             console.log(element.innerText.trim().replace(/\s/g, " "));
         });
     }
+    function education() {
+        const education = document.querySelectorAll('#app-container > section.bg-color-background-container.education-container.py-2.pl-2.mt-1.collapsible-list-container > ol > li');
+        // const experiences = document.querySelectorAll('span');
+        education.forEach(element => {
+            console.log(element.innerText.trim().replace(/\s/g, " "));
+        });
+    }
     experiences();
+    education();
 }
 
 window.addEventListener("load", scrapeText);
