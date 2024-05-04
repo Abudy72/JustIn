90f6231e807fa1551fe0cfd5717a1620ff7258ae
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-05-04 14:44:23 -0400

    Store data

--- JustIn/content.js
+++ JustIn/content.js
@@ -1,22 +1,56 @@
 async function scrapeText() {
-    function selectText(selector) {
-        const experiences = document.querySelectorAll(selector);
-        experiences.forEach(element => {
-            console.log(element.innerText.trim().replace(/\s/g, " "));
-        });
+  function selectText(selector) {
+    const elements = document.querySelectorAll(selector);
+    if (elements.length > 0) {
+      elements.forEach((element) => {
+        console.log(element.innerText.trim().replace(/\s/g, " "));
+      });
+    } else {
+      console.log("No elements found with the selector: " + selector);
     }
-    // experiences
-    console.log("experiences");
-    selectText('#app-container > section.bg-color-background-container.experience-container.py-2.pl-2.mt-1.collapsible-list-container.collapsed > ol > li');
-    // education
-    console.log("education");
-    selectText('#app-container > section.bg-color-background-container.education-container.py-2.pl-2.mt-1.collapsible-list-container > ol > li');
-    // activity
-    console.log("activity");
-    selectText('#app-container > section.activity-section.bg-color-background-container.mt-1');
-    // about
-    console.log("about");
-    selectText('#app-container > section.relative.about-section.bg-color-background-container.p-2.pr-0.mt-1');
+  }
+  // experiences
+  console.log("experiences");
+  selectText(
+    "#app-container > section.bg-color-background-container.experience-container.py-2.pl-2.mt-1.collapsible-list-container.collapsed > ol > li",
+  );
+  // education
+  console.log("education");
+  selectText(
+    "#app-container > section.bg-color-background-container.education-container.py-2.pl-2.mt-1.collapsible-list-container > ol > li",
+  );
+  // activity
+  console.log("activity");
+  selectText(
+    "#app-container > section.activity-section.bg-color-background-container.mt-1",
+  );
+  // about
+  console.log("about");
+  selectText(
+    "#app-container > section.relative.about-section.bg-color-background-container.p-2.pr-0.mt-1",
+  );
+}
+
+function saveText(json_data) {
+  chrome.storage.sync.set(json_data, function () {
+    console.log("Data saved");
+  });
+}
+
+function readText(user) {
+  chrome.storage.sync.get([user], function (items) {
+    console.log(items);
+    return items;
+  });
 }
 
 window.addEventListener("load", scrapeText);
+test_dict = {
+  theycallmeswift: {
+    name: "Mike Swift",
+    position: "Software Engineer",
+    company: "MLH",
+  },
+};
+saveText(test_dict);
+console.log(readText("theycallmeswift"));
