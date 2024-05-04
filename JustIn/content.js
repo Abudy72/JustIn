67717b7b13246641c096a6dd889a5ff87a1787c6
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-05-04 12:17:56 -0400

    Refactor

--- JustIn/content.js
+++ JustIn/content.js
@@ -1,20 +1,22 @@
 async function scrapeText() {
-    function experiences() {
-        const experiences = document.querySelectorAll('#app-container > section.bg-color-background-container.experience-container.py-2.pl-2.mt-1.collapsible-list-container.collapsed > ol > li');
-        // const experiences = document.querySelectorAll('span');
+    function selectText(selector) {
+        const experiences = document.querySelectorAll(selector);
         experiences.forEach(element => {
             console.log(element.innerText.trim().replace(/\s/g, " "));
         });
     }
-    function education() {
-        const education = document.querySelectorAll('#app-container > section.bg-color-background-container.education-container.py-2.pl-2.mt-1.collapsible-list-container > ol > li');
-        // const experiences = document.querySelectorAll('span');
-        education.forEach(element => {
-            console.log(element.innerText.trim().replace(/\s/g, " "));
-        });
-    }
-    experiences();
-    education();
+    // experiences
+    console.log("experiences");
+    selectText('#app-container > section.bg-color-background-container.experience-container.py-2.pl-2.mt-1.collapsible-list-container.collapsed > ol > li');
+    // education
+    console.log("education");
+    selectText('#app-container > section.bg-color-background-container.education-container.py-2.pl-2.mt-1.collapsible-list-container > ol > li');
+    // activity
+    console.log("activity");
+    selectText('#app-container > section.activity-section.bg-color-background-container.mt-1');
+    // about
+    console.log("about");
+    selectText('#app-container > section.relative.about-section.bg-color-background-container.p-2.pr-0.mt-1');
 }
 
 window.addEventListener("load", scrapeText);
