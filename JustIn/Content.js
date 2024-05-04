c3ccdb36622a361a1ad4e72ce0984dda5655a442
Julien-ser <73262183+Julien-ser@users.noreply.github.com>
2024-05-04 01:36:47 -0400

    Add files via upload

--- JustIn/Content.js
+++ JustIn/Content.js
@@ -1,15 +1,21 @@
 function run(){
-  console.log(document.URL);
-  var iFrame  = document.createElement("iframe");
-  iframe.style.height = "300px";
-  iframe.style.width = "100%";
-  iframe.style.position = "fixed";
-  iframe.style.bottom = "0";
-  iframe.style.left = "0";
-  iframe.style.border = "none";
-  iframe.style.zIndex = "1000";
-  iFrame.src  = chrome.runtime.getURL("hullo.html");
-  document.body.appendChild (iframe);
+  var url = document.URL
+  if(url.includes('linkedin')){
+
+    /*var body = document.getElementsByTagName('body')[0];
+    body.innerHTML = '';*/
+    var iFrame  = document.createElement("iframe");
+    /*iFrame.style.height = "300px";
+    iFrame.style.width = "100%";
+    iFrame.style.position = "fixed";
+    iFrame.style.bottom = "0";
+    iFrame.style.left = "0";
+    iFrame.style.border = "none";
+    iFrame.style.zIndex = "1000";*/
+    iFrame.src = chrome.runtime.getURL("hullo.html");
+    document.body.appendChild(iFrame);
+
+  }
 }
 
 window.onload = (e) => {  
