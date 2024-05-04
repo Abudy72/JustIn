029df91a829d6ed48d530b33e12e145b020f61f9
Julien-ser <julien.serbanescu@gmail.com>
2024-05-04 10:58:19 -0400

    Testing

--- JustIn/popup.js
+++ JustIn/popup.js
@@ -1,3 +1,6 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(JSON.stringify(process.env.GOOGLE_GEN_AI_KEY));
 document.addEventListener('DOMContentLoaded', function() {
     var tab = document.getElementById('expandable-tab');
 
@@ -5,3 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
         tab.classList.toggle('hidden');
     });
 });
+
+async function run(prompt) {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
